0b53c8d4d501b616980cc59fc85ec35afae23211
Ryan P. Brewster <RyanPBrewster@gmail.com>
2022-10-20 10:50:06 -0400

    do a scan

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -6,11 +6,18 @@ extern "C" {
 
 #[no_mangle]
 pub extern "C" fn add(a: i32, b: i32) -> i32 {
-    log(b"reading 'hello'");
-    let mut value = [0; 16];
-    let n = get(b"hello", &mut value);
-    unsafe { *value.get_unchecked_mut(n) = b'o'; }
-    set(b"hello", unsafe { value.get_unchecked(..=n) });
+    for i in 2.. {
+        if !is_prime(i) {
+            continue;
+        }
+        let key = format!("hello-{}", i);
+        log(format!("reading key={}", key).as_bytes());
+        let cur = get(key.as_bytes());
+        if cur.is_empty() {
+            set(key.as_bytes(), b"here");
+            break;
+        }
+    }
     a + b
 }
 
@@ -26,13 +33,46 @@ fn set(key: &[u8], value: &[u8]) {
     }
 }
 
-fn get(key: &[u8], value: &mut [u8]) -> usize {
+fn get(key: &[u8]) -> Vec<u8> {
+    let mut value = Vec::with_capacity(1_024);
     unsafe {
-        host_get(
+        let len = host_get(
             key.as_ptr(),
             key.len(),
             value.as_mut_ptr(),
-            value.len(),
-        ) as usize
+            value.capacity(),
+        );
+        value.set_len(len as usize);
+    }
+    log(format!("read {:?}={:?}", key, value).as_bytes());
+    value
+}
+
+fn is_prime(n: u32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if n % 2 == 0 {
+        return n == 2;
+    }
+    let mut i = 3;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        println!("{} % {} > 0", n, i);
+        i += 2;
+    }
+    true
+}
+
+#[cfg(test)]
+mod test {
+    use crate::is_prime;
+
+    #[test]
+    fn is_prime_smoke_test() {
+        let primes: Vec<u32> = (1..20).filter(|&i| is_prime(i)).collect();
+        assert_eq!(primes, [2, 3, 5, 7, 11, 13, 17, 19]);
     }
 }
